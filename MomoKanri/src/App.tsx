e513d6e1f2b50de70883ae7269cd04029bfda826
Maureen-GONTHIER <maureen.gonthier@oclock.school>
2024-11-26 15:26:48 +0900

    Project Start

---
+++ MomoKanri/src/App.tsx
@@ -0,0 +1,12 @@
+import './Styles/App.css'
+
+export default function App() {
+
+  return (
+    <div className="container mx-auto px-10 bg-zinc-300">
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>    
+    </div>
+  )
+}
